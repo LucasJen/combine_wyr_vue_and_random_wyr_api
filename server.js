6c0fa3d4a35e88cde9e96218c8815de6baa376be
LucasJen <fake@fake.com>
2026-04-24 15:02:24 -0500

    deployed

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 // loads the express module
 const express = require('express')
+const path = require('path')
 
 // imports the router that was created in index.js
 const indexRouter = require('./routes/index')
 // creates main express application instance
 const app = express()
+
+// assign the location of the static files
+const staticFilePath = path.join(__dirname, 'client', 'dist')
+// tell express to treat the file location as the static files
+const staticFiles = express.static(staticFilePath)
+app.use('/', staticFiles)
 
 
 app.use('/', indexRouter)
